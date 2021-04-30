d261beeee28aeb35d8718d7de0d623021a453be5
edutsi <nilson.upis@gmail.com>
2021-04-29 23:28:53 -0300

    começando o curso

---
+++ regesCweb/src/main/java/com/devdvdd/regesCweb/RegesCwebApplication.java
@@ -0,0 +1,13 @@
+package com.devdvdd.regesCweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegesCwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegesCwebApplication.class, args);
+	}
+
+}
